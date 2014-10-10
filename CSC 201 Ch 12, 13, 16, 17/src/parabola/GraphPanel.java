8fe240df7b90fe75a3768c7cc2fc3ced450ea807
Destroierrr9 <Destroierrr9@Destroierrr9-HP>
2014-10-10 08:56:40 -0400

    Assignment 5

--- CSC 201 Ch 12, 13, 16, 17/src/parabola/GraphPanel.java
+++ CSC 201 Ch 12, 13, 16, 17/src/parabola/GraphPanel.java
@@ -6,10 +6,12 @@ public class GraphPanel extends JPanel{
 	protected void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
-		g.drawLine(	15, 0, 15, 300);//draws y line
-		g.drawString("Y",5,10);//Put Y next to y line
-		g.drawLine(0, 250, 300, 250);//draws x line
-		g.drawString("X",275,262);//Put x next to x line
+		g.drawLine(200, 0, 200, 300);//draws y line
+		g.drawString("Y",190,10);//Put Y next to y line
+		g.drawLine(0, 200, 300, 200);//draws x line
+		g.drawString("X",275,212);//Put x next to x line
+		g.drawString("X^2 Parabola", 15, 15);//title, draws string 
+		g.drawLine(15,15,86,15);//Underlines Title 
 		double scaleFactor = 0.1;
 		Polygon p = new Polygon();//makes polygon
 		//adds points to polygon
